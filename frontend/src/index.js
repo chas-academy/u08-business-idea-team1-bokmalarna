bae4464ad7d813e4a5237077dd691e294bb873bf
dinler91 <ahmet.dinler@chasacademy.se>
2022-05-12 13:53:28 +0200

    fix routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login } from './components/login/Login';
+import { Registration } from './components/registration/Registration';
+import { Search } from './components/search/Search';
+import { Dashboard } from './components/dashboard/Dashboard';
+import { Bookpage } from './components/bookpage/Bookpage';
+import { Addbook } from './components/addbook/Addbook';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />}  />
+      <Route path="/register" element={<Registration />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/bookpage" element={<Bookpage />} />
+      <Route path="/addbook" element={<Addbook />} />
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
