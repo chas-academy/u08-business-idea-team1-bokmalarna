80f2255e7a24b3d9398815b2488a999052db30d5
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-05-16 16:22:43 +0200

    install cors for backend, let user login from frontend to backend and recive a token in localstorage

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,11 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 8081;
 const userRouter = require("./routes/user");
+var cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,7 +1,83 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 export const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const userData = {
+      email,
+      password,
+    };
+
+    login(userData);
+  };
+
+  const API_URL = "http://localhost:8080/user/";
+
+  const login = async (userData) => {
+    const response = await axios.post(API_URL + "login", userData);
+
+    if (response.data) {
+      localStorage.setItem("user", JSON.stringify(response.data));
+      console.log(response.data);
+    }
+
+    return response.data;
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <section>
+        <h1>Login</h1>
+      </section>
+      <section>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-controll"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-controll"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter your password"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
