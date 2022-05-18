5eee97bda9b994112535e293a22a5b2399f90902
amawre <amanda.wrede@chasacademy.se>
2022-05-18 08:54:59 +0200

    add markup for bookpage.js

--- frontend/src/components/bookpage/Bookpage.js
+++ frontend/src/components/bookpage/Bookpage.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Nav from '../nav/nav';
 
 export const Bookpage = () => {
   return (
-    <div>Bookpage</div>
+    <div>
+      <Nav />
+
+      <section>
+        <h2>Book Title</h2>
+        <div>Image</div>
+        <h3>Description:</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in lacus enim. 
+          Aenean auctor bibendum tempor. Ut quis rhoncus ipsum. Nunc eget nunc quis nulla 
+          imperdiet ullamcorper. Donec commodo mi blandit, luctus sapien id, facilisis nisl. 
+          Nullam rutrum diam a mattis vulputate.</p>
+
+        <p>Author: Ugglan Ugglansson</p>
+        <p>Genre: Thriller</p>
+        <p>Condition: Crappy</p>
+        <p>Release Date: 10/05/2030</p>
+
+        <p>Owned by: Ugglemor</p>
+
+        <button>Borrow</button>
+      </section>
+    </div>
   )
 }
