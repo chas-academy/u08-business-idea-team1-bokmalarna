0b49ba58086ab57693884b4c1eeca685479070a4
fbarfvestam <filip.barfvestam@chasacademy.se>
2022-05-18 14:03:04 +0200

    add bootstrap in index.html add and start with static design dashboard

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <section className="container">
+      <h1>Welcome Ugglan</h1>
+      <p>"A book is a gift you can open again and again"</p>
+      <button className="btn btn-outline-secondary">New Messages</button>
+      <button className="btn btn-outline-secondary">Settings</button>
+      <section className="card"></section>
+    </section>
+  );
 };
