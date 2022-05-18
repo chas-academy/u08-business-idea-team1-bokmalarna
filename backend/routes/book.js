805b4fc3557ac3d4a246c751e4d792ffd2047070
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-05-18 19:56:46 +0200

    Add logic to create new book

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -3,5 +3,15 @@ const router = express.Router()
 const Book = require("../models/book")
 
 
+// Create book
+router.post("/", async (req, res) => {
+  try {
+    const book = new Book(req.body)
+    await book.save()
+    res.json(book)
+  } catch (error) {
+    res.json({message: "Could not create book"})
+  }
+})
 
 module.exports = router;
