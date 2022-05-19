887e54d0a2760e3a3787429db711d2460af03d9c
frinica <frida.nicander@chasacademy.se>
2022-05-19 14:53:18 +0200

    add form layout to register a new user

--- frontend/src/components/registration/Registration.js
+++ frontend/src/components/registration/Registration.js
@@ -1,7 +1,96 @@
-import React from 'react'
+import React from "react";
 
 export const Registration = () => {
   return (
-    <div>Registration</div>
-  )
-}
+    <>
+      <section>
+        <h1>Registration</h1>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <div className="form-floating mb3">
+              <input
+                type="text"
+                className="form-control"
+                id="firstName"
+                name="firstName"
+                value={firstName}
+                onChange={onChange}
+              />
+              <label for="floatingInput">First name</label>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <div className="form-floating mb3">
+              <input
+                type="text"
+                className="form-control"
+                id="lastName"
+                name="lastName"
+                value={lastName}
+                onChange={onChange}
+              />
+              <label for="floatingInput">Last name</label>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <div className="form-floating mb3">
+              <input
+                type="text"
+                className="form-control"
+                id="city"
+                name="city"
+                value={city}
+                onChange={onChange}
+              />
+              <label for="floatingInput">City of residence</label>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <div className="form-floating mb3">
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                value={email}
+                onChange={onChange}
+              />
+              <label for="floatingInput">Email</label>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <div className="form-floating mb3">
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                name="password"
+                value={password}
+                onChange={onChange}
+              />
+              <label for="floatingInput">Password</label>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <div className="form-floating mb3">
+              <input
+                type="password"
+                className="form-control"
+                id="confirmPassword"
+                name="confirmPassword"
+                value={confirmPassword}
+                onChange={onChange}
+              />
+              <label for="floatingInput">Confirm Password</label>
+            </div>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
