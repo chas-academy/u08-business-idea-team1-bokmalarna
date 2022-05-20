fe5c6fd5199a0ef2c6dff02851930453ac2d4dd1
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-05-20 09:03:25 +0200

    fix responsive design

--- frontend/src/components/search/Search.js
+++ frontend/src/components/search/Search.js
@@ -3,9 +3,9 @@ import React from 'react'
 export const Search = () => {
   return (
   <section className='container my-5 rounded'>
-    <section className='my-5'>
+    <section className='my-5 row'>
       <form className='d-flex flex-column justify-content-center' action="">
-        <div className="mb-3">
+        <div className="mb-3 col">
           <label htmlFor="formGroupExampleInput" className="form-label">Search...</label>
           <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Search for a book" />
         </div>
@@ -27,27 +27,29 @@ export const Search = () => {
         </div>
       </form>
     </section>
-    <section className='my-5 d-flex justify-content-center'>
-      <div className="card shadow-lg mx-4 rounded" style={{width: "18rem"}}>
-        <img src="https://i.imgur.com/hJE4Jfx.jpg" className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Title: Arthas</h5>
-          <p className="card-text">Owner: Ugglan</p>
-          <div className='d-flex justify-content-between align-items-center pt-4'>
-            <a href="#" className="btn btn-primary">Loan</a>
-            <a href="#" className="btn btn-primary">Visit Book</a>
+    <section className='my-5 container align-items-center'>
+      <div className="row align-items-center">  
+        <div className="card shadow-lg mx-4 rounded" style={{width: "18rem"}}>
+          <img src="https://i.imgur.com/hJE4Jfx.jpg" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Title: Arthas</h5>
+            <p className="card-text">Owner: Ugglan</p>
+            <div className='d-flex justify-content-between align-items-center pt-4'>
+              <a href="#" className="btn btn-primary">Loan</a>
+              <a href="#" className="btn btn-primary">Visit Book</a>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="card shadow-lg mx-4 rounded" style={{width: "18rem"}}>
-        <img src="https://i.imgur.com/hJE4Jfx.jpg" className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Title: Arthas</h5>
-          <p className="card-text">Owner: Ugglan</p>
-          <div className='d-flex justify-content-between align-items-center pt-4'>
-            <a href="#" className="btn btn-primary">Loan</a>
-            <a href="#" className="btn btn-primary">Visit Book</a>
-          </div>
+         </div>
+        <div className="card shadow-lg mx-4 rounded" style={{width: "18rem"}}>
+          <img src="https://i.imgur.com/hJE4Jfx.jpg" className="card-img-top" alt="..." />
+           <div className="card-body">
+            <h5 className="card-title">Title: Arthas</h5>
+            <p className="card-text">Owner: Ugglan</p>
+            <div className='d-flex justify-content-between align-items-center pt-4'>
+              <a href="#" className="btn btn-primary">Loan</a>
+               <a href="#" className="btn btn-primary">Visit Book</a>
+            </div>
+           </div>
         </div>
       </div>
     </section>
