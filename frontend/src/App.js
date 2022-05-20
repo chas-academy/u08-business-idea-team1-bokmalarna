1d76ad883a8c925b726f159a84c3f931b119d7a0
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-05-20 12:09:05 +0200

    create Home component and move all home related code from app.js to home.js/ installed react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,41 +7,14 @@ import { Registration } from './components/registration/Registration';
 import { Search } from './components/search/Search';
 import { Nav } from './components/nav/nav';
 import { Footer } from './components/footer/Footer';
+import { Home } from './components/Home/Home';
 
 function App() {
 	return (
-		<div className="App bg-books">
-			<header>
-				<Nav />
-			</header>
-			<main className="my-5">
-				<section className="container shadow bg-body rounded my-5 opacity-75">
-					<h1 className="text-center pt-5">Welcome To</h1>
-					<div className="d-flex justify-content-center my-5">
-						<img
-							src="https://i.imgur.com/S8WCatY.png"
-							alt="BookOwl Logo"
-						/>
-					</div>
-					<h2 className="text-center mt-5">BookOwl</h2>
-					<p className="text-center">
-						Here you can borrow books from other people or loan your
-						books to other people
-					</p>
-					<div className="text-center my-5">
-						<h3>Get started by</h3>
-						<p className="pb-5">
-							Sign in
-							<br />
-							or
-							<br />
-							Register
-						</p>
-					</div>
-				</section>
-			</main>
+		<>
+			<Home />
 			<Footer />
-		</div>
+		</>
 	);
 }
 
