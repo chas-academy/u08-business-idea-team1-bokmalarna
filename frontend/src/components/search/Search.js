ac6ee024ff2a68e4a26cd854bf765ff7b2ae43d4
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-05-20 08:32:36 +0200

    Finalize static search site design

--- frontend/src/components/search/Search.js
+++ frontend/src/components/search/Search.js
@@ -28,20 +28,26 @@ export const Search = () => {
       </form>
     </section>
     <section className='my-5 d-flex justify-content-center'>
-      <div className="card" style={{width: "18rem"}}>
+      <div className="card shadow-lg mx-4 rounded" style={{width: "18rem"}}>
         <img src="https://i.imgur.com/hJE4Jfx.jpg" className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
+          <h5 className="card-title">Title: Arthas</h5>
+          <p className="card-text">Owner: Ugglan</p>
+          <div className='d-flex justify-content-between align-items-center pt-4'>
+            <a href="#" className="btn btn-primary">Loan</a>
+            <a href="#" className="btn btn-primary">Visit Book</a>
+          </div>
         </div>
       </div>
-            <div className="card" style={{width: "18rem"}}>
+      <div className="card shadow-lg mx-4 rounded" style={{width: "18rem"}}>
         <img src="https://i.imgur.com/hJE4Jfx.jpg" className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
+          <h5 className="card-title">Title: Arthas</h5>
+          <p className="card-text">Owner: Ugglan</p>
+          <div className='d-flex justify-content-between align-items-center pt-4'>
+            <a href="#" className="btn btn-primary">Loan</a>
+            <a href="#" className="btn btn-primary">Visit Book</a>
+          </div>
         </div>
       </div>
     </section>
