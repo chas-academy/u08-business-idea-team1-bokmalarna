10df0c33e1846022246630981bd1867a60115628
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-05-20 10:29:02 +0200

    create a static representation off the landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import { Addbook } from './components/addbook/Addbook';
 import { Bookpage } from './components/bookpage/Bookpage';
@@ -9,11 +8,39 @@ import { Search } from './components/search/Search';
 import { Nav } from './components/nav/nav';
 
 function App() {
-  return (
-    <div className="App">
-      <Nav />
-    </div>
-  );
+	return (
+		<div className="App bg-books mh-100">
+			<header>
+				<Nav />
+			</header>
+			<main className="my-5">
+				<section className="container shadow bg-body rounded my-5 opacity-75">
+					<h1 className="text-center pt-5">Welcome To</h1>
+					<div className="d-flex justify-content-center my-5">
+						<img
+							src="https://i.imgur.com/S8WCatY.png"
+							alt="BookOwl Logo"
+						/>
+					</div>
+					<h2 className="text-center mt-5">BookOwl</h2>
+					<p className="text-center">
+						Here you can borrow books from other people or loan your
+						books to other people
+					</p>
+					<div className="text-center my-5">
+						<h3>Get started by</h3>
+						<p className="pb-5">
+							Sign in
+							<br />
+							or
+							<br />
+							Register
+						</p>
+					</div>
+				</section>
+			</main>
+		</div>
+	);
 }
 
 export default App;
