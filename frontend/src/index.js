946c560b4c635063eb9fef4129eb6493d9c327f6
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-05-20 13:02:34 +0200

    fix a functionall navbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,34 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Login } from './components/login/Login';
-import { Registration } from './components/registration/Registration';
-import { Search } from './components/search/Search';
-import { Dashboard } from './components/dashboard/Dashboard';
-import { Bookpage } from './components/bookpage/Bookpage';
-import { Addbook } from './components/addbook/Addbook';
-import Nav from './components/nav/nav';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<Nav />
-			<Routes>
-				<Route path="/" element={<App />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/register" element={<Registration />} />
-				<Route path="/search" element={<Search />} />
-				<Route path="/dashboard" element={<Dashboard />} />
-				<Route path="/bookpage" element={<Bookpage />} />
-				<Route path="/addbook" element={<Addbook />} />
-			</Routes>
-		</BrowserRouter>
-	</React.StrictMode>
+	<>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</>
 );
 
 // If you want to start measuring performance in your app, pass a function
