ee794087f65dcf805686e71c7cfee76f76eedb5f
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-05-23 19:22:44 +0300

    update jwt token sign for login function route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -47,6 +47,9 @@ router.post("/login", async (req, res) => {
         {
           id: user._id,
           email: user.email,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          city: user.city,
         },
         "YOUR_SECRET_KEY"
       );
