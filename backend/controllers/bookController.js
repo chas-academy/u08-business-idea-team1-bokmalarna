ab289c6431b2116ef08b81b58a5f46f40fa628fc
fbarfvestam <filip.barfvestam@chasacademy.se>
2022-05-25 13:20:05 +0200

    create uploads folder for img

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,2 +1,12 @@
 const multer = require("multer");
-const Book = require("./models/book");
+const Book = require("../models/book");
+const uploadImg = multer({ storage: storage }).single("image");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
