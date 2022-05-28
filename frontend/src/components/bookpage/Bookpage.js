69d10efd3e0eae77b9854b5e4be71e9d352764f9
frinica <frida.nicander@chasacademy.se>
2022-05-28 15:18:46 +0200

    update bookpage component with dynamic text

--- frontend/src/components/bookpage/Bookpage.js
+++ frontend/src/components/bookpage/Bookpage.js
@@ -5,16 +5,16 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 
 export const Bookpage = () => {
-  const [book, setBook] = useState();
+  const [book, setBook] = useState({});
 
   const API_URL = "https://bookowl-backend.herokuapp.com/book/";
 
   const getBook = async () => {
     const { id } = useParams();
     await axios.get(API_URL + { id }).then((res) => {
-      if (res.data.book) {
-        console.log(res.data.book);
-        setBook(res.data.book);
+      if (res.data) {
+        console.log(res.data);
+        setBook(res.data);
       }
     });
   };
@@ -27,7 +27,7 @@ export const Bookpage = () => {
     <div className="lightbrownbg">
       <section className="container text-center p-5">
         <div className="card shadow-lg d-flex align-items-center">
-          <h2 className="m-4 fw-bold">Book Title</h2>
+          <h2 className="m-4 fw-bold">{book.title}</h2>
           <img
             className="rounded mx-auto d-block m-3"
             src="https://www.kathrynlasky.com/assets/covers/owl3.jpg"
@@ -36,18 +36,12 @@ export const Bookpage = () => {
           />
           <h3>Description:</h3>
           <div className="container-sm">
-            <p className="card-body mx-5">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in
-              lacus enim. Aenean auctor bibendum tempor. Ut quis rhoncus ipsum.
-              Nunc eget nunc quis nulla imperdiet ullamcorper. Donec commodo mi
-              blandit, luctus sapien id, facilisis nisl. Nullam rutrum diam a
-              mattis vulputate.
-            </p>
+            <p className="card-body mx-5">{book.description}</p>
 
-            <p>Author: Ugglan Ugglansson</p>
-            <p>Genre: Thriller</p>
-            <p>Condition: Crappy</p>
-            <p>Release Date: 10/05/2030</p>
+            <p>Author: {book.author}</p>
+            <p>Genre: {book.genre}</p>
+            <p>Condition: {book.condition}</p>
+            <p>Release Date: {book.released}</p>
 
             <p>Owned by: Ugglemor</p>
 
