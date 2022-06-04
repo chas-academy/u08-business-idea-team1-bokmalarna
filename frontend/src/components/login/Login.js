070129a48171b16bf78f6e2d1249cbfe32c80599
frinica <frida.nicander@chasacademy.se>
2022-06-04 21:54:31 +0200

    save changes after pull

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,89 +1,89 @@
-import React from 'react';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Cookies from 'js-cookie';
-import axios from 'axios';
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+import axios from "axios";
 
 export const Login = () => {
-	const navigate = useNavigate();
-	const user = Cookies.get('access_token');
+  const navigate = useNavigate();
+  const user = Cookies.get("access_token");
 
-	const [formData, setFormData] = useState({
-		email: '',
-		password: '',
-	});
-	const { email, password } = formData;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
 
-	const onChange = (e) => {
-		setFormData((prevState) => ({
-			...prevState,
-			[e.target.name]: e.target.value,
-		}));
-	};
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-	const onSubmit = (e) => {
-		e.preventDefault();
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-		const userData = {
-			email,
-			password,
-		};
+    const userData = {
+      email,
+      password,
+    };
 
-		login(userData);
-	};
+    login(userData);
+  };
 
-	//Login function
-	const login = async (userData) => {
-		const response = await axios.post(
-			process.env.REACT_APP_API_URL + 'user/login',
-			userData
-		);
-		if (response.data.token) {
-			Cookies.set('access_token', response.data.token);
-			console.log(response.data);
-			navigate('/dashboard');
-			window.location.reload();
-		}
-	};
+  //Login function
+  const login = async (userData) => {
+    const response = await axios.post(
+      process.env.REACT_APP_API_URL + "user/login",
+      userData
+    );
+    if (response.data.token) {
+      Cookies.set("access_token", response.data.token);
+      console.log(response.data);
+      navigate("/dashboard");
+      window.location.reload();
+    }
+  };
 
-	return (
-		<>
-			<section>
-				<h1>Login</h1>
-			</section>
-			<section>
-				<form onSubmit={onSubmit}>
-					<div className="form-group">
-						<input
-							type="email"
-							className="form-controll"
-							id="email"
-							name="email"
-							value={email}
-							placeholder="Enter your email"
-							onChange={onChange}
-						/>
-					</div>
+  return (
+    <>
+      <section>
+        <h1>Login</h1>
+      </section>
+      <section>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-controll"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            />
+          </div>
 
-					<div className="form-group">
-						<input
-							type="password"
-							className="form-controll"
-							id="password"
-							name="password"
-							value={password}
-							placeholder="Enter your password"
-							onChange={onChange}
-						/>
-					</div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-controll"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter your password"
+              onChange={onChange}
+            />
+          </div>
 
-					<div className="form-group">
-						<button type="submit" className="btn btn-block">
-							Submit
-						</button>
-					</div>
-				</form>
-			</section>
-		</>
-	);
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
