41bc1e0e5b27840ddab25baa2de6bb7f28593ad3
frinica <frida.nicander@chasacademy.se>
2022-06-04 22:21:25 +0200

    add test for registering a new user

---
+++ frontend/cypress/e2e/register.cy.js
@@ -0,0 +1,18 @@
+describe("register.cy.js", () => {
+  it("succesfully loads", () => {
+    cy.visit("/");
+    cy.contains("Register").click();
+    cy.url().should("include", "register");
+    cy.get('[id="firstName"]').type("Happy").should("have.value", "Happy");
+    cy.get('[id="lastName"').type("Tester").should("have.value", "Tester");
+    cy.get('[id="city"').type("Utopia").should("have.value", "Utopia");
+    cy.get('[id="email"')
+      .type("happy@my.test")
+      .should("have.value", "happy@my.test");
+    cy.get('[id="password"').type("happy123").should("have.value", "happy123");
+    cy.get('[id="confirmPassword"')
+      .type("happy123")
+      .should("have.value", "happy123");
+    cy.contains("Register User").click();
+  });
+});
