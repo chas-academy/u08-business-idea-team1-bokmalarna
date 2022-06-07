d80fa6930fbb96de17c66e61f45c5d26383baccc
frinica <frida.nicander@chasacademy.se>
2022-06-07 22:57:48 +0200

    update function in BE to store and get messages in a conversation

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -2,4 +2,29 @@ const express = require("express");
 const router = express.Router();
 const Message = require("../models/message");
 
+// Add message
+router.post("/", async (req, res) => {
+  const newMsg = new Message(req.body);
+
+  try {
+    const savedMsg = await newMsg.save();
+    res.status(200).json(savedMsg);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Get messages
+
+router.get("/:conversationId", async (req, res) => {
+  try {
+    const messages = await Message.find({
+      conversationId: req.params.conversationId,
+    });
+    res.status(200).json(messages);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
