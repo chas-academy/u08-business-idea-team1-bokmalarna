4a40cf140d2fbf1d1f27f73156cd9924b594568d
frinica <frida.nicander@chasacademy.se>
2022-06-07 23:22:43 +0200

    Update messenger to get the users id and the users conversations

--- frontend/src/components/messenger/Messenger.js
+++ frontend/src/components/messenger/Messenger.js
@@ -1,10 +1,57 @@
-import React from "react";
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import Cookies from "js-cookie";
 import "../../App.css";
 import { ChatOnline } from "./ChatOnline";
 import { Conversation } from "./Conversaton";
 import { Message } from "./Message";
+import { useNavigate } from "react-router-dom";
 
 export const Messenger = () => {
+  const navigate = useNavigate();
+  const userToken = Cookies.get("access_token");
+  const [user, setUser] = useState({});
+  const [conversations, setConversations] = useState([]);
+
+  const checkUser = async () => {
+    //User sends its access_token in headers to BE to be decoded.
+    await axios
+      .get(process.env.REACT_APP_API_URL + "user/protected", {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      })
+      .then((res) => {
+        if (res.data.user) {
+          //Stores user info into the state.
+          setUser(res.data.user);
+        }
+      });
+  };
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    } else {
+      checkUser();
+    }
+  }, [user.id]);
+
+  useEffect(() => {
+    const getConversations = async () => {
+      try {
+        const res = await axios.get(
+          process.env.REACT_APP_API_URL + "conversations/" + user.id
+        );
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getConversations();
+  }, [user.id]);
+
   return (
     <>
       <div className="messenger">
