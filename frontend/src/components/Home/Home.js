f5e0b126c1ed3add026237faffbe6153704d5569
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-08 22:50:53 +0200

    change home component login + register to sign in + sign up and add link to browse books

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,25 +1,38 @@
 import React from 'react';
 
 export const Home = () => {
-  return (
-    <main className="App bg-books py-5 px-5">
-      <section className="mainPage w-75 m-auto">
-        <div className="mainPage shadow bg-body rounded bg-opacity-75">
-        <h1 className="text-center pt-4">Welcome to BookOwl!</h1>
-        <div className="d-flex justify-content-center">
-          <img src="bookowl.png" className="m-3" style={{ width: 110 }} />
-        </div>
-        <p className="text-center fs-4 m-2 p-2">
-          BookOwl is a platform where you can loan books from other people as
-          well as loan out your books to other people.
-        </p>
-        <p className="text-center fs-4 m-2 p-3 text-decoration-none">
-          Get started by <a href="/register" className='text-decoration-none'>Creating an account</a>{' '}
-					or <a href="/login" className='text-decoration-none'> login </a>
-					from the menu! or browsing books.
-        </p>
-        </div>
-      </section>
-    </main>
-  );
+	return (
+		<main className="App bg-books py-5 px-5">
+			<section className="mainPage w-75 m-auto">
+				<div className="mainPage shadow bg-body rounded bg-opacity-75">
+					<h1 className="text-center pt-4">Welcome to BookOwl!</h1>
+					<div className="d-flex justify-content-center">
+						<img
+							src="bookowl.png"
+							className="m-3"
+							style={{ width: 110 }}
+						/>
+					</div>
+					<p className="text-center fs-4 m-2 p-2">
+						BookOwl is a platform where you can loan books from
+						other people as well as loan out your books to other
+						people.
+					</p>
+					<p className="text-center fs-4 m-2 p-3 text-decoration-none">
+						Get started by{' '}
+						<a href="/register" className="text-decoration-none">
+							Sign Up
+						</a>{' '}
+						or{' '}
+						<a href="/login" className="text-decoration-none">
+							{' '}
+							Sign In{' '}
+						</a>
+						here or from the menu! or you can brows some{' '}
+						<a href="/search">books here!</a>.
+					</p>
+				</div>
+			</section>
+		</main>
+	);
 };
