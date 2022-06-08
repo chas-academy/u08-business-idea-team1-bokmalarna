7a7454beb191f4101d18534e8a3774035ebad14e
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-06-08 17:57:09 +0200

    removed useNavigation from registration.js

--- frontend/src/components/registration/Registration.js
+++ frontend/src/components/registration/Registration.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 export const Registration = () => {
-	const navigate = useNavigate();
 	const [formData, setFormData] = useState({
 		firstName: '',
 		lastName: '',
