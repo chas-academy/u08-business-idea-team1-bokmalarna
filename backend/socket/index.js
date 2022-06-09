facf3ee692f1fba3326dc9db75bb22d70ed5a72b
frinica <frida.nicander@chasacademy.se>
2022-06-09 13:44:01 +0200

    remove console logs in index.js

--- backend/socket/index.js
+++ backend/socket/index.js
@@ -10,7 +10,6 @@ const io = require("socket.io")(8900, {
 let users = [];
 
 const addUser = (userId, socketId) => {
-  console.log("Users array", users);
   !users.some((user) => user.userId === userId) &&
     users.push({ userId, socketId });
 };
@@ -20,7 +19,6 @@ const removeUser = (socketId) => {
 };
 
 const getUser = (userId) => {
-  /* console.log(userId); */
   console.log("Get user", users);
   return users.find((user) => user.userId === userId);
 };
@@ -31,7 +29,6 @@ io.on("connection", (socket) => {
 
   //take userId and socketId from user
   socket.on("addUser", (userId) => {
-    console.log("on add user", userId);
     addUser(userId, socket.id);
     io.emit("getUsers", users);
   });
@@ -39,9 +36,6 @@ io.on("connection", (socket) => {
   //send and get message
   socket.on("sendMessage", ({ senderId, receiverId, text }) => {
     const user = getUser(receiverId);
-    console.log("Text", text);
-    console.log("Sender", senderId);
-    console.log("Receiver", receiverId);
     io.to(user.socketId).emit("getMessage", {
       senderId,
       text,
