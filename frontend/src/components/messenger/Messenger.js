f00f1b16f5d379b64534af02ff5126698d6de7c1
frinica <frida.nicander@chasacademy.se>
2022-06-09 14:15:32 +0200

    update message.js to display conversation members names dynamically

--- frontend/src/components/messenger/Messenger.js
+++ frontend/src/components/messenger/Messenger.js
@@ -58,7 +58,7 @@ export const Messenger = () => {
     });
   }, []);
 
-  // Messages only displays in relevant chat
+  // Messages only displays in relevant chat and updates chat in real time
   useEffect(() => {
     arrivalMessage &&
       currentChat?.members.includes(arrivalMessage.sender) &&
@@ -155,7 +155,12 @@ export const Messenger = () => {
                 <div className="chatBoxTop">
                   {messages.map((m) => (
                     <div ref={scrollRef}>
-                      <Message message={m} own={m.sender === user.id} />
+                      <Message
+                        message={m}
+                        own={m.sender === user.id}
+                        conversation={currentChat}
+                        currentUser={user}
+                      />
                     </div>
                   ))}
                 </div>
